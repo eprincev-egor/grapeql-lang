55c9a4843fb4b107d921555c2b69b41e5441c653
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2019-02-16 12:17:23 +0300

    coverage 100%

--- tests/syntax/CaseWhen-spec.js
+++ tests/syntax/CaseWhen-spec.js
@@ -19,6 +19,19 @@ describe("CaseWhen", () => {
     });
 
     testSyntax(CaseWhen, {
+        str: "case when (true) then 1 end",
+        result: {
+            case: [
+                {
+                    when: {elements: [{boolean: true}]},
+                    then: {elements: [{number: "1"}]}
+                }
+            ],
+            else: null
+        }
+    });
+
+    testSyntax(CaseWhen, {
         str: "case when 'some' then (1+1) when true or false then (1+1) else -2 end",
         result: {
             case: [
