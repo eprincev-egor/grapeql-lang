c574cd929ac0de29d2cf28785d75b2876a062026
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2019-02-17 14:04:28 +0300

    +WindowItem

---
+++ tests/syntax/WindowItem-spec.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const WindowItem = require("../../lib/syntax/WindowItem");
+const testSyntax = require("../testSyntax");
+
+describe("WindowItem", () => {
+
+    testSyntax(WindowItem, {
+        str: "x as (order by company.name)",
+        result: {
+            as: {word: "x", content: null},
+            body: {
+                windowDefinition: null,
+                partitionBy: null,
+                orderBy: [
+                    {
+                        expression: {elements: [
+                            {star: false, link: [
+                                {word: "company", content: null},
+                                {word: "name", content: null}
+                            ]}
+                        ]},
+                        vector: "asc",
+                        using: null,
+                        nulls: null
+                    }
+                ],
+                range: null,
+                rows: null
+            }
+        }
+    });
+
+});
